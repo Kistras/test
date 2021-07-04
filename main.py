fdb491473191f691b849329fb2132b6eba29b4c9
Kistras <81854722+Kistras@users.noreply.github.com>
2021-07-05 00:37:35 +0500

    Now it like does math

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("123")
+
+print("321")
+
+num = int(input("Input your number: "))
+if num <= 0:
+	print("Number should be bigger than 0.")
+	exit()
+
+while num != 0:
+	if num % 2 == 0:
+		num /= 2
+	else:
+		num -= 1
+	print(num)
